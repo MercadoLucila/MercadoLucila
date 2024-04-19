525251115ebf4e296f050cfa04edd30449e4c35d
Luz <mercadoruizlucila@hotmail.com>
2024-04-19 20:18:33 -0300

    Hacemos tp del mapa

--- LucilaMercadoRuiz/TP4/json.js
+++ LucilaMercadoRuiz/TP4/json.js
@@ -1,8 +1,14 @@
 function boton() {
-    fetch ("https://randomuser.me/api/")
+    const urlrandomuser="https://randomuser.me/api/";
+    fetch (urlrandomuser)
         .then (response => response.json())
-        .then (data => {
+        .then (data =>{
             console.log(data)
-
+        
+            let longitude=data.results[0].location.coordinates.longitude
+            let latitude=data.results[0].location.coordinates.latitude
+            let name=data.results[0].name
+            
+            console.log(name)
         })
 }
