8564807bb00973dd1d06babb0fb91b8b7e739094
Luz <mercadoruizlucila@hotmail.com>
2024-05-23 19:54:23 -0300

    tarea para casa

--- LucilaMercadoRuiz/POO1 copy/JS/fn2.js
+++ LucilaMercadoRuiz/POO1 copy/JS/fn2.js
@@ -1,14 +1,33 @@
 class Persona{
-    constructor(name,lastname,gender,city,email){
+    constructor(name,lastname,gender,city,email,fecha_nacimiento){
         this.name=name;
         this.lastname=lastname;
         this.gender=gender;
         this.city=city;
         this.email=email;
+        this.fecha_nacimiento=fecha_nacimiento;
+    }
+
+    getEdad(edad){
+        let anio = new Date();
+        let anioactual = [
+            anio.getFullYear(),
+            anio.getMonth(),
+            anio.getDate()];
+
+        
+        console.log(anioactual)
+        edad = (anioactual - this.fecha_nacimiento)
+        return edad
+        console.log(edad)
     }
 
     saludar(){
         console.log(`hola ${this.name}`)
+    }
+
+    queEdad(){
+        console.log(`${this.name} tiene ${edad} años.`)
     }
 
 }
@@ -29,22 +48,30 @@ class Empleado extends Persona{
     }
 }
 
-function buscar(){
+function buscar(edad){
       let url="https://randomuser.me/api/";
       fetch(url)
         .then(response => response.json())
         .then(data =>{
-        
-            let edad = data.results[0].dob.age;
+            let fechaAPI = data.results[0].dob.date;
+            let recortefecha =fechaAPI.substring(0,10);
+            let separarfecha=recortefecha.split("-",3);
+            var fechadef=Number(separarfecha)
+
+            console.log(fechadef)
+
+            var usuario = new Persona (data.results[0].name.first,data.results[0].name.last,data.results[0].gender,data.results[0].location.city,separarfecha)
+            
+            edad=usuario.getEdad(edad)
 
             console.log(edad) 
         
-            if (edad<18){
-                var usuario = new Cliente (data.results[0].name.first,data.results[0].name.last,data.results[0].gender,data.results[0].location.city,data.results[0].email)
-                }
+            if (edad<30){
+                var usuario = new Cliente (data.results[0].name.first,data.results[0].name.last,data.results[0].gender,data.results[0].location.city,data.results[0].email,fecha)
+                 }
 
-            if (edad>18){
-                var usuario = new Empleado (data.results[0].name.first,data.results[0].name.last,data.results[0].gender,data.results[0].location.city,data.results[0].email)   
+            if (edad>30){
+                var usuario = new Empleado (data.results[0].name.first,data.results[0].name.last,data.results[0].gender,data.results[0].location.city,data.results[0].email,fecha)   
                 }
             
                 console.log(usuario)
