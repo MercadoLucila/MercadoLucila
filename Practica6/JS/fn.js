e66db5259f23444654d43e13785157a811fa91f2
Luz <mercadoruizlucila@hotmail.com>
2024-05-28 23:55:54 -0300

    práctica funcionando

--- Practica6/JS/fn.js
+++ Practica6/JS/fn.js
@@ -28,44 +28,50 @@ class Cuenta_Bancaria extends Cliente{
     }
 
     get consultar_saldo(){
+        
+        document.getElementById("resultadoC").innerHTML=`<p>Saldo actual de ${this.saldo}</p>`  
         return this.saldo;
     }
 
     set depositar(monto){
+        if (monto>1.000 && monto>=1){
         this.saldo = this.saldo + monto;
-        document.getElementById("resultadoD").innerHTML=`<p>Extracción del monto de ${monto} exitoso. Saldo total de ${this.saldo}</p>`
-        
-        return saldo
+        document.getElementById("resultadoD").innerHTML=`<p>Depósito del monto de ${monto} exitoso. Saldo actual de ${this.saldo}</p>`
+        } else {
+            document.getElementById("resultadoD").innerHTML=`<p>El monto ingresado es inaceptable. Puede depositar un mínimo de $1 hasta $1000. Inténtelo nuevamente con un monto válido.</p>`
+        }  
+         
+        return this.saldo
     }
 
     set retirar(monto){
         if (monto>this.saldo){
-            document.getElementById("resultadoR").innerHTML=`<p>El monto que desea extraer supera el saldo. Intente con un monto menor</p>` 
+            document.getElementById("resultadoR").innerHTML=`<p>El monto que desea extraer supera el saldo actual. Intente con un monto menor</p>` 
         } else {
             this.saldo = this.saldo - monto;
-            document.getElementById("resultadoR").innerHTML=`<p>Extracción del monto de ${monto} exitoso. Saldo total de ${this.saldo}</p>`
-            return saldo
+            document.getElementById("resultadoR").innerHTML=`<p>Extracción del monto de ${monto} exitoso. Saldo actual de ${this.saldo}</p>`  
         }
-        
+
+        return this.saldo
     }
 
 }
 
 var usuario
 
-function registrar(cliente){
+function registrar(){
     let nombre = document.getElementById("name").value;
     let apellido = document.getElementById("surname").value;
     let fecha_nac = document.getElementById("birthdate").value;
 
-    var usuario = new Cliente(nombre,apellido,fecha_nac)
+    usuario = new Cliente(nombre,apellido,fecha_nac)
     
     let edadUsuario= usuario.getEdad();
     
     if (edadUsuario > 18) {
         let saldo = 0;
-        var cliente = new Cuenta_Bancaria(nombre,apellido,fecha_nac,edadUsuario,saldo)
-        console.log(cliente)
+        usuario = new Cuenta_Bancaria(nombre,apellido,fecha_nac,edadUsuario,saldo)
+        console.log(usuario)
         document.getElementById("resultado").innerHTML=`<p>Nueva Cuenta Bancaria registrada con éxito</p>`
 
         document.getElementById("montoDepositado").disabled = false;
@@ -75,22 +81,22 @@ function registrar(cliente){
     } else {
         document.getElementById("resultado").innerHTML=`<p>Al ser menor de edad no puedes crear una cuenta bancaria aún.</p>` 
     }
-    depositarMonto(cliente)
+    
+    
 }
 
-function depositarMonto(cliente){
-    let monto = document.getElementById("montoDepositado").value;
-        if (monto>1.000){
-            cliente.depositar = monto
-        } else {
-            document.getElementById("resultadoD").innerHTML=`<p>El monto ingresado es inaceptable. Puede depositar un mínimo de $1 hasta $1000. Inténtelo nuevamente con un monto válido.</p>`
-        }  
+function depositarMonto(){
+    var monto = Number(document.getElementById("montoDepositado").value);
+    usuario.depositar = monto
 }
 
 function retirarMonto(){
-    let monto = document.getElementById("montoRetirado").value;
-        cliente.retirar(monto)
+    let monto = Number(document.getElementById("montoRetirado").value);
+    usuario.retirar = monto
+}
 
+function consultarSaldo(){
+    usuario.consultar_saldo
 }
 
 /*Cuando guardamos los objetos o información en el Storage de JavaScript
